8f942c1dbd2162c630709654e9fe69813baacdef
Nick Scoles <scolesnick@gmail.com>
2019-04-15 15:47:30 -0500

    Update WorkerTaskGraph.java
    added method to get least common labels

--- src/research/crowdsourcing/WorkerTaskGraph.java
+++ src/research/crowdsourcing/WorkerTaskGraph.java
@@ -1670,6 +1670,33 @@ public class WorkerTaskGraph
             }
             return result;
         }
+	public HashMap getLeastCommonLabels(){
+            int numOfTasks = getTasks().size();
+            HashMap<String, Integer> result = new HashMap();
+//            int result[] = new int[numOfTasks];           
+            for(int n = 0; n < numOfTasks; n++){ 
+                AnalyzedTask task = getTasks().get(n);                
+                ArrayList<AnalyzedWorker> associatedWorkers = allWorkersForTask(task);
+                HashMap<String, Integer> labelCounts = new HashMap();
+                for(int k = 0; k < associatedWorkers.size(); k++){
+                    int label = labelFor(associatedWorkers.get(k), task);
+                    Integer count = labelCounts.get(""+label);
+                    if(count == null) labelCounts.put(""+label,new Integer(1));
+                    else labelCounts.put(""+label,new Integer(count+1));
+                }
+                int lowestVal = 1000000;
+                String leastCommon = "";
+                for(HashMap.Entry<String,Integer> entry : labelCounts.entrySet()){
+                    if(entry.getValue() < lowestVal){                        
+                        lowestVal = entry.getValue();
+                        leastCommon = entry.getKey();
+                    }
+                }
+                result.put(""+task.getId(),Integer.parseInt(leastCommon));
+            }
+            return result;
+        }
+	
         public HashMap getKeyMap(){
             int numOfTasks = getTasks().size();
             HashMap<String, Integer> result = new HashMap();
